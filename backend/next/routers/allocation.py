e4225c70de4599a86a88c294f85faca678f6cbc9
rhijviii <rhijvidhungel@gmail.com>
2026-06-29 02:39:24 +0545

    updated backend for examhall

--- backend/next/routers/allocation.py
+++ backend/next/routers/allocation.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
