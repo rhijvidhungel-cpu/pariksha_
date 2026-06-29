a9d825cb485452385f1cff69b45234a20141b6f8
rhijviii <rhijvidhungel@gmail.com>
2026-06-29 10:44:26 +0545

    updated backend for examhall

--- backend/next/main.py
+++ backend/next/main.py
@@ -1,6 +1,4 @@
-import io
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import traceback
 import pandas as pd
 
--- backend/next/routers/allocation.py
+++ backend/next/routers/allocation.py
@@ -1,13 +1,9 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
 from models import Student, ExamHall
-from schemas import ExamHallCreate
-
+from backend.next.schemas import ExamHallCreate
 router = APIRouter(tags=["Allocation"])
 
 @router.post("/")
