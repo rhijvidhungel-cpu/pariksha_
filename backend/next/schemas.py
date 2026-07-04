e1187a37c06f31f0134f23ac9589e257e2086776
rhijviii <rhijvidhungel@gmail.com>
2026-07-04 23:40:12 +0545

    added batches pages

--- backend/next/schemas.py
+++ backend/next/schemas.py
@@ -1,14 +1,14 @@
 from typing import Optional
 from pydantic import BaseModel
 
+
 class ExamHallCreate(BaseModel):
     room_no: str
     rows_count: int
     benches_per_row: int
     seats_per_bench: int
-    capacity: Optional[int] = None # Allows the backend to accept the request
+    capacity: Optional[int] = None
 
-    from pydantic import BaseModel
 
 class BatchResponse(BaseModel):
     batch_id: int
